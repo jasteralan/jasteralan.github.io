880014958de0b4978aa4fc8d0daafec4d7cecb41
jasteralan <jaster1019@gmail.com>
2021-01-11 09:49:49 +0000

    Deploying to gh-pages from  @ b36f6f61584d8dfaa0a272455b1503f44aadbb85 🚀

--- _next/static/IMYUArikU9_j3LHHjeC_k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fd2215cd91129451d5cc1.css","static\u002Fchunks\u002Fpages\u002Findex-c1643e4ac6e5c48ab553.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e2ffa4674eea62b50f3f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
